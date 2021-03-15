960e2c267c69a192e2f7e62a922081ed6cc64d82
Katreque <rverissimo56@gmail.com>
2021-03-15 20:54:04 -0300

    Revert "Issue #1 done."

--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
-require('dotenv').config();
+// const Discord = require("discord.js");
 
-const Discord = require('discord.js');
-const client = new Discord.Client();
+function sum(a, b) {
+	return a + b;
+}
 
-const { checkDiscordInfo } = require('./initHandler');
-
-checkDiscordInfo(process.env.DISCORD_TOKEN, process.env.DISCORD_ID);
-
-client.on('ready', () => {
-	console.log(`${client.user.username} is swimming.`);
-	console.log(`Your Discord bot invite: https://discord.com/oauth2/authorize?client_id=${process.env.DISCORD_ID}&scope=bot&permissions=8`);
-});
-
-client.login(process.env.DISCORD_TOKEN);
+module.exports = sum;
--- tests/initHandler.test.js
+++
@@ -1,9 +0,0 @@
-const { checkDiscordInfo } = require("../src/initHandler");
-
-test('Check if func is throwing correctly.', () => {
-	let throwMessage = "DISCORD_ID or DISCORD_TOKEN must be in the .env file.";
-
-    expect(() => {checkDiscordInfo("ABC", "")}).toThrowError(throwMessage);
-	expect(() => {checkDiscordInfo("", "ABC")}).toThrowError(throwMessage);
-	expect(() => {checkDiscordInfo("", "")}).toThrowError(throwMessage);
-});
